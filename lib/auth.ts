36fa4f196ec01df80581c3d9ce208205ea8b22c3
Edrian Miguel E. Capistrano <edrianmiguelcapistrano@gmail.com>
2025-11-14 10:12:03 +0800

    edit schema

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,6 +1,6 @@
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
-import { db } from "@/db/drizzle"; // your drizzle instance
+import { db } from "@/db/drizzle"; 
 import { schema } from "@/db/schema";
 import { nextCookies } from "better-auth/next-js";
 
